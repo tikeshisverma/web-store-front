8d2ccf56fb4014ec297e98348c6ef2cae96ba656
tikeshisverma <54203846+tikeshisverma@users.noreply.github.com>
2020-06-21 20:00:43 +0530

    frontend

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+  <div class="wrapper">
+  <input v-model="storeName" placeholder="store name">
+  <input v-model="storeId" placeholder="store id">
+<select v-model="storeType">
+   <option disabled value="">Store type</option>
+   <option>kirana store</option>
+   <option>home busine</option>
+   <option>boutique</option>
+   <option>multi brand S</option>
+   <option>mall</option>
+</select>
+<select v-model="storeCategory">
+   <option disabled value="">Store type</option>
+   <option>cooked food</option>
+   <option>grocery</option>
+   <option>fashion</option>
+   <option>yoga</option>
+   <option>dance</option>
+</select>
+<textarea v-model="storeDiscription" placeholder="store discription"></textarea>
+<button class="click" @click="onclick" >creat store front</button>
+ </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+  components: {
+   
+  },
+   data() {
+     return{
+     storeType:"",
+     storeDiscription:"",
+     storeName:"",
+     storeId:"",
+     storeCategory:"",
+
+   }
+   },
+   methods:{
+     onclick(){
+       const payload = {
+         store_name: this.storeName,
+         store_id: this.storeId,
+         store_type: this.storeType,
+         store_category : this.storeCategory,
+         store_discription: this.storeDiscription
+       }
+       console.log("data-->",payload)
+       this.creatStore(payload)
+     },
+    creatStore(payload){
+      fetch('http://localhost:3000/api/store', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+      .then(res => res.json())
+      .then(res => console.log("dfasdfsdfsdf", res))
+    } 
+   },
+   
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+input, select, textarea, button{
+  margin: 10px;
+}
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+}
+</style>
